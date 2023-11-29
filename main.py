e40b71ded70fcd2ff55df24029b323459e437227
Blowsight71345 <idiot71345@gmail.com>
2023-11-29 22:50:29 +0300

    add 2 files: sp_gr.txt and zadaniya.txt update tg_bot_theme: the user enters a last name. If it is in the file, the user receives the task

--- main.py
+++ main.py
@@ -2,30 +2,71 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-bot = Bot(token="Your token")
+try:
+    with open("sp_gr.txt", "r", encoding="utf-8") as file:
+        all_student = [line.rstrip("\n") for line in file]
+        all_student = list(filter(None, all_student))
+except FileNotFoundError:
+    print(f"Запрашиваемый файл 'sp_gr.txt' не найден")
+
+try:
+    with open("zadaniya.txt", "r", encoding="utf-8") as file:
+        zadaniya = [line.rstrip("\n") for line in file]
+        zadaniya = list(filter(None, zadaniya))
+except FileNotFoundError:
+    print(f"Запрашиваемый файл 'zadaniya.txt' не найден")
+
+bot = Bot(token="your token")
 dp = Dispatcher(bot)
+
+HELP_COMMAND = """
+Этот бот даст каждому студенту даст задание для зачета :) 
+Нажмите /start, чтобы начать работу.
+/help - список команд 🫠
+/start - начать работу с ботом ❤️
+/rules - правила для всех ✨ 
+"""
+
+
+@dp.message_handler(commands=["help"])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMAND)
+
+
+@dp.message_handler(commands=["rules"])
+async def rules_command(message: types.Message):
+    await message.answer(
+        text="Правила для всех: \nТелеграм-бот должен быть написан на aiogram и выложен на github в публичной директории \n"
+             "В боте должны быть доступны команды: \n/start - начать работу с ботом \n/help - список команд \n"
+             "Обязательны следующие пункты: использование кнопок, работа с файлами, обработка исключений. Удачи!")
+    await message.delete()
+
+
+@dp.message_handler(commands=["help"])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMAND)
 
 
 @dp.message_handler(commands=["start"])
-async def process_start_command(message: types.Message):
-    kb = [[types.KeyboardButton(text="Рандомное число")],
-          [types.KeyboardButton(text="Что-то")]]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-    await message.reply("Привет, я бот в тг", reply_markup=keyboard)
-
-
-@dp.message_handler(lambda message: message.text == "Рандомное число")
-async def process_button_click(message: types.Message):
-    import random
-
-    a = random.randint(1, 100)
-    await bot.send_message(message.from_user.id, str(a))
+async def start_command(message: types.Message):
+    await message.answer(text="Введите свою фамилию, чтобы бот дал Вам задание.")
 
 
 @dp.message_handler()
-async def echo_message(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
+async def send_zadanie(message: types.Message):
+    import random
+
+    z = random.choice(zadaniya)
+    found = False
+    for name in all_student:
+        if name.startswith(message.text):
+            found = True
+            break
+    if found:
+        await message.answer(text=z)
+    else:
+        await message.answer(text="Такого студента нет в списке. Введите фамилию с большой буквы")
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
